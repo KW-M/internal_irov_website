376bf40f76fe8a5c96c384ae79833bfecf1c5e04
Ryan Anderson <rkanderson15@gmail.com>
2023-05-22 18:34:07 -0700

    updated websocketRelay with more robust reconnection and error handling code, as well as connection status tracking. Comments added.

--- backend/js/websocketRelay.ts
+++ backend/js/websocketRelay.ts
@@ -1,42 +1,77 @@
 
+
+/*
+    Wrapper class for the WebSocket built-in javascript module. Acts as a proxy for communication
+    between the iROV internal website and its python code. At the time of writing this comment (5/22/2023)
+    the URL for the websocket server that the iROV python is expected to host is ws://localhost:8765/
+*/
 export class WebSocketRelay {
     
-    ws: WebSocket
+    socket: WebSocket
+    msgReceivedFn: (msgEvent: MessageEvent<Uint8Array>) => void 
+    isRunning: boolean
+    isConnected: boolean
     
-    constructor() {
-        this.ws = new WebSocket("ws://localhost:8765/");
-        // can send messages with this.ws.send("yourmessage")
-        // TODO accept an arbitrary callback function as a parameter.
+    /*
+        When instantiated, an arbitrary callback function is expected to handle incoming messages
+        from the iROV. The boolean flag isConnected is used to keep track of the connection status
+        and is set to false by default. isRunning is used to track whether or not the relay should actively
+        try to connect/reconnect (in the event of an error or the socket getting closed.) By default, this
+        is false and is set to true only when the user activates the relay using the start() method.
+    */
+    constructor(msgReceivedFn: (msgEvent: MessageEvent<Uint8Array>) => void) {
+        this.msgReceivedFn = msgReceivedFn;
+        this.isConnected = false
+        this.isRunning = false
     }
 
+    /*
+        This function MUST be invoked for the iROV to even start reaching out to connect.
+        After it is called, it will continue attempting to maintain connection unless stop()
+        is invoked.
+    */
     start() {
-        this.ws.addEventListener("open", this.onOpen);
-        this.ws.addEventListener("message", this.onMessageReceived);
-        this.ws.addEventListener("close", this.onClose);
-        this.ws.addEventListener("error", this.onError);
+        this.isRunning = true
+        this.connect()
     }
 
-    onOpen(event: Event) {
-
+    stop() {
+        this.isRunning = false
+        this.socket.close()
     }
 
-    onMessageReceived(event: MessageEvent<any>) {
-        //event.data is how you can access the message
-        // a callback should be triggered (an arbitrary function passed in to the constructor.)
+    /*
+        Meant to be used internally. Connection and Reconnection both occur through instantiating a new
+        WebSocket and assigning callbacks. Arbitrary delays occur in the event of a di
+    */
+    connect() {
+        this.socket = new WebSocket("ws://localhost:8765/");
+        this.isConnected = true
+        this.socket.addEventListener('close', (event) => {
+          console.log('WebSocket connection closed with code:', event.code);
+          this.isConnected = false
+          if(this.isRunning) setTimeout(this.connect, 2000); // Attempt to reconnect after a delay
+        });
+      
+        this.socket.addEventListener('error', (error) => {
+          console.error('WebSocket error:', error);
+          this.isConnected = false
+          if(this.isRunning) setTimeout(this.connect, 2000); // Attempt to reconnect after a delay
+        });
+
+        this.socket.addEventListener("message", this.msgReceivedFn)
+      
     }
 
-    onClose(event: CloseEvent) {
-        // should try to reconnect
+    getIsConnected() {
+        return this.isConnected
     }
 
-    onError(event: Event) {
-        // Log the error
-    }
-
-    // TODO should add an is_connected function to query if the server is connecetd
-
+    /*
+        Sends an arbitrary byte sequence through the WebSocket
+    */
     sendMessage(message: Uint8Array) {
-        this.ws.send(message)
+        this.socket.send(message)
     }
 
 
